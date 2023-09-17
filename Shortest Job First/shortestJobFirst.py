dcdb06c04463ab6082f9e2a5f104dcfc1fcbd25b
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-09-17 19:52:33 +0500

    feat: add shortest remaning time first

--- Shortest Job First/shortestJobFirst.py
+++ Shortest Job First/shortestJobFirst.py
@@ -1,8 +1,10 @@
 from random import randint
+from statistics import mean
 from texttable import Texttable
 import plotly.express as px
 import pandas as pd
 from datetime import datetime
+
 
 class Process:
     def __init__(self, process_id, arrival_time, burst_time, execution_time):
@@ -58,11 +60,33 @@ def input_entity(entity: str, min: int, max: int):
 
 def print_process_table(process_list):
     table = Texttable()
-    table_rows = [["process_id", "arrival_time", "burst_time", "completion_time", "turn_around_time", "wait_time", "response_time", "utilization_time"]]
+    table_rows = [["process_id", "arrival_time", "burst_time", "completion_time",
+                   "turn_around_time", "wait_time", "response_time", "utilization_time"]]
     for process in process_list:
         new_row = [process.process_id, process.arrival_time, process.burst_time,
                    process.completion_time, process.turn_around_time, process.wait_time, process.response_time, process.utilization_time]
         table_rows.append(new_row)
+    table.add_rows(table_rows)
+    table.set_max_width(200)
+    print(table.draw())
+
+
+def print_process_average_table(process_list):
+    table = Texttable()
+    table_rows = [["average_completion_time",
+                   "average_turn_around_time", "average_wait_time", "average_response_time", "average_utilization_time"]]
+    average_completion_time = mean(
+        [process.completion_time for process in process_list])
+    average_turn_around_time = mean(
+        [process.turn_around_time for process in process_list])
+    average_wait_time = mean([process.wait_time for process in process_list])
+    average_response_time = mean(
+        [process.response_time for process in process_list])
+    average_utilization_time = mean(
+        [process.utilization_time for process in process_list])
+
+    new_row = [average_completion_time, average_turn_around_time, average_wait_time, average_response_time, average_utilization_time]
+    table_rows.append(new_row)
     table.add_rows(table_rows)
     table.set_max_width(200)
     print(table.draw())
@@ -79,7 +103,8 @@ def draw_gantt_chart(process_list):
 
     fig = px.timeline(df, x_start="Start", x_end="Finish", y="Process")
     fig.update_yaxes(autorange="reversed")
-    fig.update_layout(xaxis=dict(title='Seconds', tickformat='%S', tickvals=pd.date_range(start=df['Start'].min(), end=df['Finish'].max(), freq='S')))
+    fig.update_layout(xaxis=dict(title='Seconds', tickformat='%S', tickvals=pd.date_range(
+        start=df['Start'].min(), end=df['Finish'].max(), freq='S')))
     fig.show()
 
 
@@ -153,17 +178,59 @@ def execute_shortest_job_first(processes):
 
     print("Final Process Table")
     print_process_table(processes)
+    print_process_average_table(processes)
 
 
 def execute_shortest_remaining_time_first(processes):
-    return
+    ready_queue = []
+    running_queue = []
+    time_passed = 0
+
+    print('\nExecuting Processes according to Shortest Remaining Time First.')
+    while check_should_execution_proceed(processes):
+        ready_queue = []
+        for process in processes:
+            if process.arrival_time <= time_passed and process.time_left > 0:
+                ready_queue.append(process)
+
+        if len(ready_queue) == 0:
+            time_passed += 1
+        else:
+            sorted_ready_queue_according_to_shortest_job = sort_processes_according_to_shortest_job(
+                ready_queue)
+            minimum_job = sorted_ready_queue_according_to_shortest_job[0].burst_time
+
+            processes_of_same_shortest_job = get_processes_of_same_shortest_job(
+                sorted_ready_queue_according_to_shortest_job, minimum_job)
+
+            sorted_ready_queue_according_to_shortest_arrival = sort_processes_according_to_shortest_arrival(
+                processes_of_same_shortest_job)
+
+            running_queue = [
+                sorted_ready_queue_according_to_shortest_arrival[0]]
+
+            ran_process = running_queue[0]
+            ran_process.set_response_time(time_passed)
+            ran_process.set_start_time(time_passed)
+            time_passed += 1
+            ran_process.time_left -= 1
+            if ran_process.time_left == 0:
+                ran_process.set_end_time(time_passed)
+                ran_process.set_completion_time(time_passed)
+                ran_process.set_turn_around_time()
+                ran_process.set_wait_time()
+                ran_process.set_utilization_time()
+
+    print("Final Process Table")
+    print_process_table(processes)
+    print_process_average_table(processes)
 
 
 if __name__ == "__main__":
     # number_of_processes = input_entity("number of processes", 3, 5)
     number_of_processes = 5
-    arrival_times = [0, 1, 2, 3, 4]
-    burst_times = [10, 1, 2, 1, 5]
+    arrival_times = [2, 5, 1, 0, 4]
+    burst_times = [6, 2, 8, 3, 4]
     processes = []
     for i in range(number_of_processes):
         process_id = i + 1
